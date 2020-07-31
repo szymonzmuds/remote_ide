731731d792aab0ba3920a7ebb60953ff336c776c
szymon.zmuds <szymon.zmudax@gmail.com>
2020-07-31 16:39:46 +0200

    add img displaying and start creating system checkig if user is connected

--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
 var socket = io.connect('http://localhost:3000');
 
 var root = document.getElementById("root");
-
+//to root, to show active file / directory
 var active_id = null;
+//active button - to display file content
+var active_button = null;
 
 
 window.onload = function () {
@@ -35,8 +37,6 @@ socket.on('start', function (data) {
             disp.appendChild(b);
             tmp.appendChild(disp);
             root.appendChild(tmp);
-            //var br = document.createElement("br");
-            //root.appendChild(br);
         }
     }
     for(var i = 0; i < files.length; i++){
@@ -50,8 +50,6 @@ socket.on('start', function (data) {
             tmp.appendChild(disp);
             tmp.addEventListener("click", check_file);
             root.appendChild(tmp);
-            //var br = document.createElement("br");
-            //root.appendChild(br);
         }
     }
 
@@ -105,6 +103,55 @@ socket.on('explorer', function(data) {
     }
 });
 
+socket.on('img', function(data){
+    /*
+    displaying img
+    */
+    var menu = document.getElementById("disp_menu");
+    var display = document.getElementById("display");
+    //first create button to disp
+    /*
+    menu button ids - file name + "_menu"
+    file display - file name + "_file"
+    */ 
+    var menu_but = document.createElement("div");
+    menu_but.id = data["parent"] + "_menu";
+    menu_but.innerText = data["parent"];
+    menu_but.alt = data["parent"];
+
+    var x_button = document.createElement("button");
+    x_button.innerText = "x";
+    x_button.addEventListener("click", remove_file);
+    menu_but.appendChild(x_button);
+    menu_but.className = "active_but_menu";
+
+    menu.appendChild(menu_but);
+    //menu_but.addEventListener("click", set_disp);
+    /*
+    while(display.childNodes.length > 1){
+        var tmp = display.lastChild;
+        display.removeChild(tmp);
+    }
+    */
+    //alert(data["path"]);
+
+    var div = document.createElement("div");
+    var img = document.createElement("img");
+    img.src = data["path"];
+    div.id = data["parent"] + "_file";
+    div.appendChild(img);
+    if(active_button != null){
+        var prev_but = document.getElementById(active_button+"_menu");
+        prev_but.classList.remove("active_but_menu");
+        var prev_file =  document.getElementById(active_button+"_file");
+        prev_file.className = "hide_file";
+        prev_but.addEventListener("click", set_disp);
+    }
+    active_button = data["parent"];
+    div.className = "active_file";
+    display.appendChild(div);
+});
+
 
 
 
@@ -115,6 +162,17 @@ function file_explorer_onclick(){
     asking for files in directory
     */
     this.removeEventListener("click", file_explorer_onclick);
+    if(active_id == null){
+        active_id = this.id;
+        this.firstChild.className = "active";
+    }
+    else{
+        var prev = document.getElementById(active_id);
+        active_id = this.id;
+        prev.firstChild.className = "disp";
+        this.firstChild.className = "active";
+    }
+    console.log(active_id);
     var name = this.innerText; 
     var path =  "/" + this.innerText;
     var el = this.parentElement;
@@ -134,11 +192,15 @@ function remove_children(){
     removing children for Node from file explorer
     */
     element = this.parentElement;
+    this.className = "disp";
+    //alert(this.className);
+    active_id = null;
     var b = document.createElement("b");
     while(element.childNodes.length > 1){
         var tmp = element.lastChild;
         element.removeChild(tmp);
     }
+    console.log(active_id);
     window.setTimeout("element.addEventListener('click', file_explorer_onclick);", 150);
 }
 
@@ -148,5 +210,56 @@ function check_file(){
     create file checking system
     and then displaying some files, like jpeg, png, txt, py, .c etc
     */
+   this.removeEventListener("click", check_file);
+   
+   if(active_id == null){
+        active_id = this.id;
+        this.firstChild.className = "active";
+    }
+    else{
+        var prev = document.getElementById(active_id);
+        active_id = this.id;
+        prev.firstChild.className = "disp";
+        this.firstChild.className = "active";
+    }
+    var name = this.innerText; 
+    var path =  "/" + this.innerText;
+    var el = this.parentElement;
+    var id = el.id;
+    while(el.id != 'root'){
+        path = "/" + el.id + path;
+        el = el.parentElement;
+    }
+    var data = {};
+    data["path"] = path;
+    data["parent"] = this.id;
+    socket.emit('file', data);
 }
 
+function set_disp(){
+    var prev_but = document.getElementById(active_button + "_menu");
+    var prev_file = document.getElementById(active_button + "_file");
+
+    var tmp = "";
+    var i = 0;
+    while(i < this.id.length-5){
+        tmp = tmp + this.id[i];
+        i++;
+    }
+    active_button = tmp;
+    var act_but = document.getElementById(active_button + "_menu");
+    var act_file = document.getElementById(active_button + "_file");
+
+    prev_but.classList.remove("active_but_menu");
+    act_but.className = "active_but_menu";
+
+    prev_file.className = "hide_file";
+    act_file.className = "active_file";
+    this.removeEventListener("click", set_disp);
+    prev_but.addEventListener("click", set_disp);
+}
+
+
+function remove_file(){
+    alert("work");
+}
--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 const os = require('os');
 const fs = require('fs');
+const pathh = require('path');
 const express = require('express');
 const socket = require('socket.io');
 const https = require('https');
+const { setInterval } = require('timers');
 
 
 var app = express();
 var server = app.listen(3000, function () {
     console.log("listen to port 3000");
 });
-//const path = os.homedir();
-//console.log(path);
+
+//variable with all user
+var user_list = {};
+var user_loop = false;
+
 
 app.use(express.static('public'));
+app.use(express.static(pathh.join(__dirname, "/public/tmp")));
 
 var io = socket(server);
 
 
 io.on('connection', function (socket) {
     console.log(`${socket.id} is connect to the server`);
+    var user = {};
+    user["id"] = socket.id;
+    user["fotos"] = {};
+    user["alive"] = true;
+    user_list[socket.id] = user;
     var id = socket.id;
+    if(user_loop == false){
+        user_loop = true;
+        setInterval(is_user_alive, 60000);
+    }
     socket.on('start', function (data) {
         /*
         get data about workspace (default root) and send to website
@@ -120,6 +135,65 @@ io.on('connection', function (socket) {
              
         });
     });
+    socket.on('file', function(data){
+        var path = data["path"];
+        if(path.endsWith(".png") || path.endsWith(".jpg")){
+            var public = __dirname + "/public";
+            var img_path = os.homedir() + path;
+            var img_p = os.homedir() + path;
+            var img_path = pathh.relative(public, img_path);
+            console.log(data["name"]);
+            path = os.homedir() + path;
+            var dir_name = __dirname;
+            var num_slash = 0;
+            for(var i = 0; i<dir_name.length; i++){
+                if(dir_name[i]=='/'){
+                    num_slash++;
+                }
+            }
+            var tmp = "";
+            for(var i = 0; i < num_slash; i++){
+                tmp += "/.."
+            }
+
+            var new_path = __dirname + "/public" + "/tmp/" + data["parent"];
+
+            fs.copyFile(img_p, new_path, fs.constants.COPYFILE_EXCL, function(){
+                var new_data = {};
+                var img_path = "../tmp/" + data["parent"];
+                new_data["path"] = img_path;
+                new_data["parent"] = data["parent"];
+                io.to(id).emit('img', new_data);
+                console.log("send img " + data["parent"]);
+            });
+
+
+            
+            var rel_path = pathh.relative(__dirname, img_p);
+            console.log(pathh.join(__dirname, rel_path, "/.."));
+            
+            path = ".." + tmp + path;
+            
+            //sleep(500);
+            
+            
+        }
+    })
 });
+
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+function is_user_alive(){
+    console.log(user_list);
+    /*
+    for(const user of user_list){
+        console.log(user);
+    }
+    */
+}
 
 
